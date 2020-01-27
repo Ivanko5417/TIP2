ad8cd0fe255af91596c5a30d3289a529f125eac4
Nikita Solosnuk <n.solosnuk@paralect.com>
2020-01-27 20:34:20 +0300

    Add 6-8 tasks to the first lab

--- lab1/index.php
+++ lab1/index.php
@@ -67,6 +67,41 @@
       echo '<br>';
 
       echo $first . $second . $third . $fourth . $author;
+
+      echo "6. ";
+        $x = 10;
+        $y = "20 приветов";
+        echo $x + $y;
+        echo "</br>";
+
+        echo "7. ";
+        $a = true;
+        $b = false;
+
+        echo "true xor true: ";
+        echo $a xor $a;
+        echo "</br>";
+
+        echo "true xor false: ";
+        echo $a xor $b;
+        echo "</br>";
+
+        echo "false xor true: ";
+        echo $b xor $a;
+        echo "</br>";
+
+        echo "false xor false: ";
+        echo $b xor $b;
+        echo "</br>";
+
+        echo "8. ";
+        $x = 10;
+        $y = 15;
+        echo "x: $x, y: $y <br>";
+        $x += $y;
+        $y = $x - $y;
+        $x -= $y;
+        echo "x: $x, y: $y";
     ?>
   </body>
 </html>
